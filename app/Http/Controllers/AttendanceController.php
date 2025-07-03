280ef15431bb9cab57fd2ea34cdda1e047121a5b
dendendenden123 <159616363+dendendenden123@users.noreply.github.com>
2025-07-03 21:31:11 +0800

    feat(attendance): refactor searchMembers method to implement search functionality for member attendance

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Attendance;
+use App\Models\User;
 
 class AttendanceController extends Controller
 {
     public function index(Request $request)
     {
-
         $attendance = Attendance::with(['user', 'event'])
             ->whereIn('id', function ($query) {
                 $query->select(DB::raw('MAX(id)'))
-                    ->from('attendances')
-                    ->groupBy('user_id');
+                    ->from('attendances') 
+                    ->groupBy('user_id'); 
             })
             ->orderBy('created_at', 'desc')
             ->simplePaginate(5);
@@ -28,21 +28,25 @@ class AttendanceController extends Controller
 
     public function searchMembers(Request $request)
     {
-        $searchTerm = $request->input('searchTerm');
+            $search = $request->input('query');
 
-        // $attendance = Attendance::with(['user', 'event'])
-        //     ->whereHas('user', function ($query) use ($searchTerm) {
-        //         $query->where('name', 'like', "%{$searchTerm}%");
-        //     })
-        //     ->orderBy('created_at', 'desc')
-        //     ->simplePaginate(5); 
+            $attendance = Attendance::with(['user', 'event'])
+                ->whereIn('id', function ($query) {
+                    $query->select(DB::raw('MAX(id)'))
+                        ->from('attendances')
+                        ->groupBy('user_id');
+                })
+                ->whereHas('user', function ($query) use ($search) {
+                    $query->where('first_name', 'like', "%{$search}%")
+                        ->orWhere('last_name', 'like', "%{$search}%");
+                })
+                ->orderBy('created_at', 'desc')
+                ->simplePaginate(5);
 
-        $attendance = "No record found";
-
-        if ($request->ajax()) {
+           if ($request->ajax()) {
             return view("admin.attendance.member-attendance-list", compact('attendance'))->render();
         }
-
-        //return view('admin.attendance.index', compact('attendance'));
+            return view('admin.attendance.index', compact('attendance'));
+    
     }
 }
